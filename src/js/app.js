6c6b2a383068cff368d5583545dfe490122f43c4
The73756 <the73756the@gmail.com>
2023-07-24 23:57:00 +0900

    Add guide section

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,12 @@ import * as flsFunctions from "./files/functions.js";
 import "./files/sliders.js";
 import "./files/scroll.js";
 import "./files/marquee.js";
+import GraphTabs from "./files/tabs.js";
+("./files/tabs.js");
 
 flsFunctions.burger();
 flsFunctions.mediaAdaptive();
 flsFunctions.handlerAnchor();
 
 Aos.init();
+new GraphTabs("tab");
